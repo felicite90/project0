8ca46e22dc117f9857b434c87be15ee1e6a007b2
felicity <bogueyerif@gmail.com>
2024-02-17 10:41:15 +0100

    package

---
+++ user.js
@@ -0,0 +1,13 @@
+const mongoose=require('mongoose');
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/test42');
+
+const Chien = mongoose.model('Chien', {name: String, category: String});
+
+const newChien = new Chien({name: "Dogg 5", category: "Germanie"});
+console.log(newChien);
+Chien.find().then(data => console.log(data));
+
+newChien.save().then(() => console.log('Le chien a ete ajouté'))
+
